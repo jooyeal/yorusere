9603e2b24a93ae79ab58536af5cfae2297b1a14b
jooyeal <34053954+jooyeal@users.noreply.github.com>
2023-08-06 16:42:05 +0900

    fix: build error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,12 @@ import BaseModal from "./BaseModal";
 import { useDisclosure } from "@mantine/hooks";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Header() {
   const [opened, { open, close }] = useDisclosure(false);
   const session = useSession();
-  const router = useRouter();
 
-  const onClickLink = (href: string) => {
-    router.push(href).then(() => close());
-  };
   return (
     <Flex className="h-16 p-4" align="center">
       <ActionIcon variant="outline" onClick={open}>
@@ -22,15 +19,12 @@ export default function Header() {
       <BaseModal opened={opened} onClose={close} fullScreen>
         <Stack justify="space-between" h="calc(100vh - 80px)">
           <Stack>
-            <Text className="font-semibold" onClick={() => onClickLink("/")}>
+            <Link className="font-semibold" href="/">
               Home
-            </Text>
-            <Text
-              className="font-semibold"
-              onClick={() => onClickLink("/expense/add")}
-            >
+            </Link>
+            <Link href="/expense/add" className="font-semibold">
               Enter new expense
-            </Text>
+            </Link>
           </Stack>
           {session.data?.user ? (
             <Text className="font-bold" onClick={async () => await signOut()}>
